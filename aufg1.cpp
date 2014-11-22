50d63734ce141c70a2c989c8f2414381113bc4da
complexinfinity <eil.jan@gmx.de>
2014-11-22 19:50:50 +0100

    +aufg1 - 3, noch nicht ganz fertig

---
+++ aufg1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double G = .61803398874;
+
+double
+f (const double x)
+{
+  return 12 * x * x - 6 * x + 4. / 3.;
+}
+
+
+
+double
+newx (double a, double b, double c)
+{
+  if (c - b > b - a)
+    return c - G * (c - b);
+  else
+    return a + G * (b - a);
+}
+
+double
+minimize (double a, double b, double c, double epsilon)
+{
+
+
+  if (abs (a - c) < epsilon)
+    return c;
+
+  double x = newx (a, b, c);
+
+  if (b < x)
+    {
+      if (f (b) < f (x))
+	return minimize (a, b, x, epsilon);
+      else
+	return minimize (b, x, c, epsilon);
+    }
+  else
+    {
+      if (f (b) < f (x))
+	return minimize (x, b, c, epsilon);
+      else
+	return minimize (a, x, b, epsilon);
+    }
+
+}
+
+
+
+
+int
+main ()
+{
+
+  cout << "Minimum bei " << minimize (-2., 1., 4., 0.0000001) << endl;
+
+  // df/dx = 24 x - 6 == 0  =>  x = 6/24 = 0.25 == der vom numerischen Algorithmus gefundenen Nullstelle
+
+}
--- aufg4a.cpp
+++ aufg4a.cpp
@@ -4,46 +4,47 @@
 #include "anneal.h"
 using namespace std;
 
-int main()
+int
+main ()
 {
-	// (Später optimierte) Reihenfolge der Städte
-	VecInt r(8);
+  // (Später optimierte) Reihenfolge der Städte
+  VecInt r (8);
 
-	for ( int i = 0; i < 8; i++ )
-	{
-		r[i] = i;
-	}
+  for (int i = 0; i < 8; i++)
+    {
+      r[i] = i;
+    }
 
-	// x-Koordinate der einzelnen Städte
-	const double x_coords[8] = { 2, 4, 1, 5, 2, 5, 4, 1 };
-	VecDoub x(8, x_coords);
+  // x-Koordinate der einzelnen Städte
+  const double x_coords[8] = { 2, 4, 1, 5, 2, 5, 4, 1 };
+  VecDoub x (8, x_coords);
 
-	// y-Koordinate der einzelnen Städte
-	const double y_coords[8] = { 2, 2, 2, 2, 3, 0, 3, 0 };
-	VecDoub y(8, y_coords);
+  // y-Koordinate der einzelnen Städte
+  const double y_coords[8] = { 2, 2, 2, 2, 3, 0, 3, 0 };
+  VecDoub y (8, y_coords);
 
-	// Metrpolois-Algorithmus auf vorgegebene Städte anwenden (aus anneal.h)
-	Anneal myAnneal;
+  // Metrpolois-Algorithmus auf vorgegebene Städte anwenden (aus anneal.h)
+  Anneal myAnneal;
 
-	const double length = myAnneal.order(x, y, r);
+  const double length = myAnneal.order (x, y, r);
 
-	// Datei öffnen (und überschreiben)
-	ofstream file("handlungsreisender.txt", ios::trunc);
+  // Datei öffnen (und überschreiben)
+  ofstream file ("handlungsreisender.txt", ios::trunc);
 
-	// Einzelne Schritte der Reise
-	for ( int i = 0; i < 8; i++ )
-	{
-		const int k = r[i];
-		file << x[k] << '\t' << y[k] << endl;
-	}
+  // Einzelne Schritte der Reise
+  for (int i = 0; i < 8; i++)
+    {
+      const int k = r[i];
+      file << x[k] << '\t' << y[k] << endl;
+    }
 
-	// zurück zum Ausgangsort
-	file << x[r[0]] << '\t' << y[r[0]] << endl;
+  // zurück zum Ausgangsort
+  file << x[r[0]] << '\t' << y[r[0]] << endl;
 
-	// Datei schließen
-	file.close();
+  // Datei schließen
+  file.close ();
 
-	cout << "Ergebnis in handlungsreisender.txt geschrieben. Länge der Reise: " << length << endl;
+  cout << "Ergebnis in handlungsreisender.txt geschrieben. Länge der Reise: " << length << endl;
 
-	// Der Algorithmus berechnet die richtige Länge von 14 (Längeneinheiten), dies ist inklusive des Rückweges zum Ausgangsort.
+  // Der Algorithmus berechnet die richtige Länge von 14 (Längeneinheiten), dies ist inklusive des Rückweges zum Ausgangsort.
 }
